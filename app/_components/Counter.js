b338c4db749b62e70178948dd71edb41499c2927
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2026-02-11 16:56:27 +0200

    feat: set up initial Next.js project structure with basic pages, components, styles, and assets.

---
+++ app/_components/Counter.js
@@ -0,0 +1,10 @@
+"use client";
+import { useState } from "react";
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+    </div>
+  );
+}
