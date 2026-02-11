01f56130809d22dc3f5a63b6c9e1d5c566544e6a
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2026-02-11 18:08:56 +0200

    feat: implement initial application layout with header and navigation components.

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Navigation from "@/app/_components/navigation";
 import { Josefin_Sans } from "next/font/google";
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 // export const metadata = {
 //   title: "The Wild Oasis ",
@@ -28,16 +29,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer style={{ fontSize: 40 }}>
-          very descriptive footer for the wild oasis
-        </footer>
+        <Header />
+        <div className="flex-1 px-8 py-12 ">
+          <main className="max-w-7xl  mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
